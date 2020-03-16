0ada0836ab9f5fcd2011e5acc2a411e122d077fb
Azurasy <alexsiryy1@gmail.com>
2020-03-15 19:38:46 -0500

    file uploading

--- back-api/index.js
+++ back-api/index.js
@@ -1,15 +1,36 @@
 const fs = require('fs');
-const bodyParser = require('body-parser');
-const express = require('express');
 const config = JSON.parse(fs.readFileSync('./appconfig.json'));
 
+const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/', (req, res) => {
-    console.log(req.body);
+var multer = require('multer')
+
+var storage = multer.diskStorage({
+    destination: function (request, file, callback) {
+        callback(null, './uploads/');
+    },
+    filename: function (request, file, callback) {
+        //console.log(file);
+        callback(null, file.originalname)
+    }
+});
+
+var upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), function (req, res) {
+    let options = JSON.parse(req.body.options);
+    console.log(`${options.name} - ${options.exposure} - ${options.expiration}`);
+    
+    console.log(req.file.originalname)
+    res.status(204).end()
 });
 
 app.listen((process.env.port || config.port), err => {
