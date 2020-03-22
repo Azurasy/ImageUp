52efbbcac879bb1ba33957aaca0e5f34739f4b9e
Onusai <alexsiryy1@gmail.com>
2020-03-22 18:41:16 -0500

    refactor

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,15 +1,12 @@
 const path = require("path");
-require('dotenv').config({path: '../.env'});
-
-let port = `http://localhost:${process.env.PORT}`;
-console.log(port);
+require("dotenv").config({ path: "../.env" });
 
 module.exports = {
   outputDir: path.resolve(__dirname, "../server/public"),
   devServer: {
     proxy: {
       "/api": {
-        target: port
+        target: `http://localhost:${process.env.PORT}`
       }
     }
   }
