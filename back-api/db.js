f39ccceab2e49e40e0e838703d0737108eba34e9
Onusai <alexsiryy1@gmail.com>
2020-03-22 15:12:52 -0500

    fixed typo

--- back-api/db.js
+++ back-api/db.js
@@ -7,7 +7,7 @@ function init() {
         host: process.env.DB_HOST, 
         user: process.env.DB_USER, 
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABSE,
+        database: process.env.DB_DATABASE,
         connectionLimit: 5
     });
 
