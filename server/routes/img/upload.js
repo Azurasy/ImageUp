3b567062ec62eabbf89e95656b266ef3229b27b4
Onusai <alexsiryy1@gmail.com>
2020-04-19 21:10:27 -0500

    fixed user id association

--- server/routes/img/upload.js
+++ server/routes/img/upload.js
@@ -15,30 +15,36 @@ var storage = multer.diskStorage({
     callback(null, file.uuid + path.extname(file.originalname));
   },
 });
-var upload = multer({ storage: storage });
+var upload = multer({ storage });
 
-router.post('/', upload.single('image'), function (req, res) {
+router.post('/', upload.single('image'), (req, res) => {
   let options = JSON.parse(req.body.options);
+
   if (options.expiration != 0)
     options.expiration += Math.floor(new Date() / 1000);
-  const f_ext = path.extname(req.file.originalname);
-  const f_name = path.basename(req.file.originalname, f_ext);
+
+  const file_ext = path.extname(req.file.originalname);
+  const file_name = path.basename(req.file.originalname, file_ext);
+  const userId = req.userId ? req.userId : 0;
   const base_url = '/img/'; // in preperation for using external storage like aws s3 bucket
 
   Image.create({
-    user: 1,
+    userId,
     uuid: req.file.uuid,
     title: options.name,
     exposure: options.exposure,
     expiration: options.expiration,
-    file_name: f_name,
-    file_ext: f_ext,
-    base_url: base_url,
+    file_name,
+    file_ext,
+    base_url,
   })
     .then(() => {
       res.json({ uuid: req.file.uuid });
     })
-    .catch((err) => console.warn(err));
+    .catch((err) => {
+      console.warn(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
