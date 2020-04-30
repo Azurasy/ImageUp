21b124f984a9f296817adecfcc580dfab83f7812
Onusai <alexsiryy1@gmail.com>
2020-04-30 10:58:25 -0500

    gallery files

---
+++ client/src/components/gallery/Gallery.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <div class="gallery" id="gallery" ref="gallery">
+      <ImageTile
+        v-for="item in images"
+        :key="item.uuid"
+        :data="item"
+        @image_loaded="resizeAll"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+/* Credits to https://uicookies.com/css-image-galleries/ and https://codepen.io/vhanla/pen/PxjZvj
+    for the core code of this gallery */
+
+import axios from 'axios';
+import ImageTile from './ImageTile';
+
+export default {
+  name: 'Home',
+  components: {
+    ImageTile,
+  },
+  props: ['user_id'],
+  data: function() {
+    return {
+      index: -1,
+      data: [],
+      images: [],
+      gallery: null,
+    };
+  },
+  methods: {
+    fetchImages() {
+      axios
+        .get(`/api/img/recent/${this.user_id}/${this.index}`)
+        .then(res => {
+          let data = res.data.data;
+          if (data) {
+            data.forEach(img => {
+              img.loaded = false;
+              img.added = false;
+              img.downloadingImage = new Image();
+              img.downloadingImage.onload = () => {
+                //console.log(`${downloadingImage.width}x${downloadingImage.height}`);
+                img.loaded = true;
+                this.updateLoaded();
+              };
+              img.downloadingImage.src = img.base_url + img.uuid + img.file_ext;
+            });
+
+            this.data = [...this.data, ...data];
+          }
+        })
+        .catch(err => console.log(err));
+    },
+    getVal(elem, style) {
+      return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
+    },
+    getHeight(item) {
+      return item.querySelector('.content').getBoundingClientRect().height;
+    },
+    resizeAll() {
+      var altura = this.getVal(this.gallery, 'grid-auto-rows');
+      var gap = this.getVal(this.gallery, 'grid-row-gap');
+      this.gallery.querySelectorAll('.gallery-item').forEach(item => {
+        var el = item;
+        el.style.gridRowEnd =
+          'span ' + Math.ceil((this.getHeight(item) + gap) / (altura + gap));
+      });
+    },
+    updateLoaded() {
+      for (let i = 0; i < this.data.length; i++) {
+        let img = this.data[i];
+        if (img.loaded && !img.added) {
+          if (i == 0 || this.images.includes(this.data[i - 1])) {
+            this.images.push(img);
+            img.added = true;
+          }
+        }
+      }
+      this.resizeAll();
+      setTimeout(() => this.resizeAll(), 500);
+    },
+  },
+  mounted() {
+    this.gallery = this.$refs.gallery;
+    this.resizeAll();
+    window.addEventListener('resize', this.resizeAll);
+    this.fetchImages();
+  },
+};
+</script>
+
+<style>
+.gallery {
+  display: grid;
+  grid-column-gap: 8px;
+  grid-row-gap: 0px;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-auto-rows: 8px;
+}
+@media (max-width: 880px) {
+  .gallery {
+    grid-template-columns: repeat(auto-fill, minmax(30%, 1fr));
+  }
+}
+@media (max-width: 660px) {
+  .gallery .content {
+    padding: 2px;
+  }
+}
+@media (max-width: 570px) {
+  .gallery {
+    grid-template-columns: repeat(auto-fill, minmax(40%, 1fr));
+  }
+}
+</style>
---
+++ client/src/components/gallery/ImageTile.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="gallery-item">
+    <div class="content">
+      <a :href="page_url" @click.prevent="clicked">
+        <img :id="'img-' + data.uuid" src="@/assets/images/blank.png" alt="" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageTile',
+  props: ['data'],
+  data: function() {
+    return {
+      img_url: `${this.data.base_url}${this.data.uuid}${this.data.file_ext}`,
+      page_url: `/i/${this.data.uuid}`,
+    };
+  },
+  methods: {
+    clicked() {
+      this.$router.push(this.page_url);
+    },
+  },
+  mounted() {
+    let img = document.getElementById(`img-${this.data.uuid}`);
+    img.src = this.data.downloadingImage.src;
+    setTimeout(() => (img.style.visibility = 'visible'), 500);
+    this.$emit('image_loaded');
+  },
+};
+</script>
+
+<style scoped>
+img {
+  max-width: 100%;
+  border-radius: 5px;
+  transition: all 1.5s ease;
+  visibility: hidden;
+
+  -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
+  -moz-animation: fadein 2s; /* Firefox < 16 */
+  -ms-animation: fadein 2s; /* Internet Explorer */
+  -o-animation: fadein 2s; /* Opera < 12.1 */
+  animation: fadein 2s;
+}
+.content {
+  padding: 4px;
+}
+.gallery-item {
+  transition: grid-row-start 300ms linear;
+  transition: transform 300ms ease;
+  transition: all 0.5s ease;
+  cursor: pointer;
+}
+.gallery-item:hover {
+  transform: scale(1.025);
+}
+
+@keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
